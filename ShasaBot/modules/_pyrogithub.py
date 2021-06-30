6a6235fd094c2d7c2512f07f098107247faf01c0
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-30 12:05:56 +0530

    Update _pyrogithub.py

--- ShasaBot/modules/_pyrogithub.py
+++ ShasaBot/modules/_pyrogithub.py
@@ -1,8 +1,8 @@
 # © @Mr_Dark_Prince
 import aiohttp
 from pyrogram import filters
-from ShasaBot import pbot
-from ShasaBot.pyrogramee.errors import capture_err
+from GabiBraunRobot import pbot
+from GabiBraunRobot.pyrogramee.errors import capture_err
 
 
 __mod_name__ = "Github"
