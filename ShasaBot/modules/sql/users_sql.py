effcccc8ea9b2ddc32caca0f81725ac91f842c37
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-30 09:27:22 +0530

    Update users_sql.py

--- ShasaBot/modules/sql/users_sql.py
+++ ShasaBot/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from ShasaBot import dispatcher
-from ShasaBot.modules.sql import BASE, SESSION
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.sql import BASE, SESSION
 from sqlalchemy import (
     Column,
     ForeignKey,
