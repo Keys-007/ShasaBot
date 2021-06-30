9510c6d64762701c09f8d390d4234fe234fd20b1
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-30 08:30:43 +0530

    Update errors.py

--- ShasaBot/utils/errors.py
+++ ShasaBot/utils/errors.py
@@ -1,10 +1,10 @@
 import sys
 import traceback
 from functools import wraps
-from ShasaBot import pbot as app
+from GabiBraunRobot import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001236063876)
+LOG_GROUP_ID = int(--1001184858685)
 
 def split_limits(text):
     if len(text) < 2048:
