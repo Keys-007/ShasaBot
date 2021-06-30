1d04e13fd09e0cec8002662bab28b67677c49055
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-30 12:45:40 +0530

    Update dev.py

--- ShasaBot/modules/dev.py
+++ ShasaBot/modules/dev.py
@@ -5,7 +5,7 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import ShasaBot
+import GabiBraunRobot
 
 from GabiBraunRobot import dispatcher
 from GabiBraunRobot.modules.helper_funcs.chat_status import dev_plus
