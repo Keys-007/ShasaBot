19828e63ec59eba0e9a0f8b538d9b3b4de5a239d
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-30 13:00:33 +0530

    Update fun.py

--- ShasaBot/modules/fun.py
+++ ShasaBot/modules/fun.py
@@ -2,11 +2,11 @@ import html
 import random
 import time
 
-import ShasaBot.modules.fun_strings as fun_strings
-from ShasaBot import dispatcher
-from ShasaBot.modules.disable import DisableAbleCommandHandler
-from ShasaBot.modules.helper_funcs.chat_status import is_user_admin
-from ShasaBot.modules.helper_funcs.extraction import extract_user
+import GabiBraunRobot.modules.fun_strings as fun_strings
+from GabiBraunRobot import dispatcher
+from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from GabiBraunRobot.modules.helper_funcs.chat_status import is_user_admin
+from GabiBraunRobotr.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async
