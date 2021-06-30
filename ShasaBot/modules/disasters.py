4bc9f5d8e505e47b91d13656859d38c4307ebe5e
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-30 12:50:31 +0530

    Update disasters.py

--- ShasaBot/modules/disasters.py
+++ ShasaBot/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from ShasaBot import (
+from GabiBraunRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,13 +13,13 @@ from ShasaBot import (
     WOLVES,
     dispatcher,
 )
-from ShasaBot.modules.helper_funcs.chat_status import (
+from GabiBraunRobot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from ShasaBot.modules.helper_funcs.extraction import extract_user
-from ShasaBot.modules.log_channel import gloggable
+from GabiBraunRobot.modules.helper_funcs.extraction import extract_user
+from GabiBraunRobot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
